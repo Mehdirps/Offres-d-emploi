820f57864775f59037bb41a635daca2d0bd46edb
Mehdirps <mehdi.raposo77@gmail.com>
2024-03-01 14:18:52 +0100

    UP : Empty companies or offers message

--- resources/views/admin/companies.blade.php
+++ resources/views/admin/companies.blade.php
@@ -6,71 +6,76 @@
     <section class="container">
         <h1>Les entreprises</h1>
         <p>Voici la liste des entreprises enregistrées dans la base de données.</p>
-        <div class="table-responsive">
-            <table class="table table-striped">
-                <thead>
-                <tr>
-                    <th>Logo</th>
-                    <th>Nom</th>
-                    <th>Adresse</th>
-                    <th>Code postal</th>
-                    <th>Ville</th>
-                    <th>Téléphone</th>
-                    <th>Email</th>
-                    <th>Site web</th>
-                    <th>Active</th>
-                    <th>Actions</th>
-                </tr>
-                </thead>
-                <tbody>
-                @foreach($companies as $company)
+        @if(count($companies) === 0)
+            <div class="alert alert-info">Aucune entreprise n'a été enregistrée pour le moment ou ne correspond à votre recherche.</div>
+        @else
+            <div class="table-responsive">
+                <table class="table table-striped">
+                    <thead>
                     <tr>
-                        <td>
-                            @if($company->logo)
-                                <img style="width: 50px" src="{{ asset($company->logo) }}"
-                                     alt="Logo de l'entreprise {{$company->name}}">
-                            @else
-                                <p class="text-danger">Aucun</p>
-                            @endif
-                        </td>
-                        <td>{{ $company->company_name }}</td>
-                        <td>{{ $company->address }}</td>
-                        <td>{{ $company->postal_code }}</td>
-                        <td>{{ $company->city }}</td>
-                        <td>{{ $company->company_phone }}</td>
-                        <td>{{ $company->company_email }}</td>
-                        <td>{{ $company->website }}</td>
-                        <td>
-                            @if($company->active)
-                                <span class="badge bg-success">Active</span>
-                            @else
-                                <span class="badge bg-danger">Inactive</span>
-                            @endif
-                        </td>
-                        <td>
-                            <div class="btn-group" role="group">
-                                <a href="{{ route('company', [$company->slug, $company->id]) }}"
-                                   class="btn btn-primary">Voir</a>
-                                <a href="{{ route('admin.company', $company->id) }}"
-                                   class="btn btn-warning">Modifier</a>
-                                <form class="delete_company" action="{{ route('admin.company.delete', $company->id) }}" method="post"
-                                      data-company="{{ $company->company_name }}">
-                                    @csrf
-                                    @method('delete')
-                                    <input type="submit" class="btn btn-danger" value="Supprimer">
-                                </form>
-                            </div>
-                        </td>
+                        <th>Logo</th>
+                        <th>Nom</th>
+                        <th>Adresse</th>
+                        <th>Code postal</th>
+                        <th>Ville</th>
+                        <th>Téléphone</th>
+                        <th>Email</th>
+                        <th>Site web</th>
+                        <th>Active</th>
+                        <th>Actions</th>
                     </tr>
-                @endforeach
-                </tbody>
-            </table>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                {{ $companies->links('partials.pagination') }}
+                    </thead>
+                    <tbody>
+                    @foreach($companies as $company)
+                        <tr>
+                            <td>
+                                @if($company->logo)
+                                    <img style="width: 50px" src="{{ asset($company->logo) }}"
+                                         alt="Logo de l'entreprise {{$company->name}}">
+                                @else
+                                    <p class="text-danger">Aucun</p>
+                                @endif
+                            </td>
+                            <td>{{ $company->company_name }}</td>
+                            <td>{{ $company->address }}</td>
+                            <td>{{ $company->postal_code }}</td>
+                            <td>{{ $company->city }}</td>
+                            <td>{{ $company->company_phone }}</td>
+                            <td>{{ $company->company_email }}</td>
+                            <td>{{ $company->website }}</td>
+                            <td>
+                                @if($company->active)
+                                    <span class="badge bg-success">Active</span>
+                                @else
+                                    <span class="badge bg-danger">Inactive</span>
+                                @endif
+                            </td>
+                            <td>
+                                <div class="btn-group" role="group">
+                                    <a href="{{ route('company', [$company->slug, $company->id]) }}"
+                                       class="btn btn-primary">Voir</a>
+                                    <a href="{{ route('admin.company', $company->id) }}"
+                                       class="btn btn-warning">Modifier</a>
+                                    <form class="delete_company"
+                                          action="{{ route('admin.company.delete', $company->id) }}" method="post"
+                                          data-company="{{ $company->company_name }}">
+                                        @csrf
+                                        @method('delete')
+                                        <input type="submit" class="btn btn-danger" value="Supprimer">
+                                    </form>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
             </div>
-        </div>
+            <div class="row">
+                <div class="col-md-12">
+                    {{ $companies->links('partials.pagination') }}
+                </div>
+            </div>
+        @endif
     </section>
 @endsection
 
--- resources/views/admin/offers.blade.php
+++ resources/views/admin/offers.blade.php
@@ -5,58 +5,63 @@
 @section('admin_content')
     <h1>Les offres d'emploi</h1>
     <p>Voici la liste des offres d'emploi enregistrées dans la base de données.</p>
-    <div class="table-responsive">
-        <table class="table">
-            <thead>
-            <tr>
-                <th>Entreprise</th>
-                <th>Titre</th>
-                <th>Contrat</th>
-                <th>Présence</th>
-                <th>Lieu</th>
-                <th>Publiée le</th>
-                <th>Active</th>
-                <th>Actions</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($offers as $offer)
+    @if(count($offers) === 0)
+        <div class="alert alert-info">Aucune offre d'emploi n'a été enregistrée pour le moment ou ne correspond à votre recherche.</div>
+    @else
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
                 <tr>
-                    <td>
-                        <a href="{{route('company',[$offer->company->slug, $offer->company->id])}}"><img
-                                width="70px" src="{{ asset($offer->company->logo) }}"
-                                alt="Logo de {{ $offer->company->company_name }}"
-                                class="img-thumbnail"></a>
-                    </td>
-                    <td>{{ $offer->title }}</td>
-                    <td>{{ $offer->contract_type }}</td>
-                    <td>{{ $offer->location }}</td>
-                    <td>{{ $offer->city }}</td>
-                    <td>{{ $offer->created_at }}</td>
-                    <td>
-                        @if($offer->active)
-                            <span class="badge bg-success">Active</span>
-                        @else
-                            <span class="badge bg-danger">Inactive</span>
-                        @endif
-                    </td>
-                    <td>
-                        <div class="btn-group" role="group">
-                            <a href="{{route('company.offers', [$offer->slug,$offer->id])}}"
-                               class="btn btn-primary">Voir</a>
-                            <a class="btn btn-secondary" href="{{route('admin.offer', [$offer->id])}}">Edit</a>
-                            <form class="delete_offer" action="{{route('admin.offer.delete', [$offer->id])}}" method="post" data-offer="{{$offer->title}}">
-                                @csrf
-                                @method('delete')
-                                <input type="submit" class="btn btn-danger" value="Supprimer">
-                            </form>
-                        </div>
-                    </td>
+                    <th>Entreprise</th>
+                    <th>Titre</th>
+                    <th>Contrat</th>
+                    <th>Présence</th>
+                    <th>Lieu</th>
+                    <th>Publiée le</th>
+                    <th>Active</th>
+                    <th>Actions</th>
                 </tr>
-            @endforeach
-            </tbody>
-        </table>
-    </div>
+                </thead>
+                <tbody>
+                @foreach($offers as $offer)
+                    <tr>
+                        <td>
+                            <a href="{{route('company',[$offer->company->slug, $offer->company->id])}}"><img
+                                    width="70px" src="{{ asset($offer->company->logo) }}"
+                                    alt="Logo de {{ $offer->company->company_name }}"
+                                    class="img-thumbnail"></a>
+                        </td>
+                        <td>{{ $offer->title }}</td>
+                        <td>{{ $offer->contract_type }}</td>
+                        <td>{{ $offer->location }}</td>
+                        <td>{{ $offer->city }}</td>
+                        <td>{{ $offer->created_at }}</td>
+                        <td>
+                            @if($offer->active)
+                                <span class="badge bg-success">Active</span>
+                            @else
+                                <span class="badge bg-danger">Inactive</span>
+                            @endif
+                        </td>
+                        <td>
+                            <div class="btn-group" role="group">
+                                <a href="{{route('company.offers', [$offer->slug,$offer->id])}}"
+                                   class="btn btn-primary">Voir</a>
+                                <a class="btn btn-secondary" href="{{route('admin.offer', [$offer->id])}}">Edit</a>
+                                <form class="delete_offer" action="{{route('admin.offer.delete', [$offer->id])}}"
+                                      method="post" data-offer="{{$offer->title}}">
+                                    @csrf
+                                    @method('delete')
+                                    <input type="submit" class="btn btn-danger" value="Supprimer">
+                                </form>
+                            </div>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    @endif
 @endsection
 
 @section('admin_footer')
@@ -64,7 +69,7 @@
         document.querySelectorAll('.delete_offer').forEach(form => {
             form.addEventListener('submit', e => {
                 const title = e.target.getAttribute('data-offer');
-                if (!confirm('Voulez-vous vraiment supprimer l\'offre '+ title +'?')) {
+                if (!confirm('Voulez-vous vraiment supprimer l\'offre ' + title + '?')) {
                     e.preventDefault();
                 }
             });
